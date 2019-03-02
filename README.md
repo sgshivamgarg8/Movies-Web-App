c81d5a493b42b8fabcdebd350f83d163584ae46d
Shivam Garg <sgshivamgarg8@gmail.com>
2019-03-03 00:13:25 +0530

    added imdb links to trending movies and added links in poster to search result movies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Movies-Web-App
-The Ultimate Movie Search Engine to Search for Movies and TV Series
+## The Ultimate Movie Search Engine to Search for Movies and TV Series
 
-1. Install Node in your machine
+1. Install Node.js in your machine
 2. Install all the npm packages required from package.json
-3. run app using "node app.js" 
+3. Run app using "node app.js" 
--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var rp = require("request-promise");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/",function(req, res){
-    var url = "https://api.themoviedb.org/3/trending/movie/day?api_key=473523253ce1a6744f253c14043dec4f";
+var options = {
+    url: "https://api.themoviedb.org/3/trending/movie/day?api_key=473523253ce1a6744f253c14043dec4f",
+    json: true
+};
 
-    request(url, function(error,response,body){
-        if(!error && response.statusCode==200){
-            var trendingmovie = JSON.parse(body);
-            res.render("home", {trendingmovie: trendingmovie});
+app.get("/",function(req, res){
+    var id = [];
+    rp(options)
+        .then(function(data) {
+            for(var i=0;i<data.results.length;i++){
+                id.push(data.results[i].id);
+            }
+            getdetailsfromid(id);
+        });
+    
+    function getdetailsfromid(id){
+        var urls = [];
+        var imdbid = [];
+        var titles = [];
+        var posters = [];
+        for(var i=0;i<id.length;i++){
+            var url = "https://api.themoviedb.org/3/movie/" + id[i] + "?api_key=473523253ce1a6744f253c14043dec4f";
+            urls.push(url);
+            var options = {
+                url: urls[i],
+                json: true
+            };
+
+            rp(options)
+                .then(function(data) {
+                    imdbid.push(data.imdb_id);
+                    titles.push(data.title);
+                    posters.push("https://image.tmdb.org/t/p/w300" + data.poster_path);
+
+                    if(titles.length == 20){
+                        res.render("home", {
+                            titles: titles,
+                            imdbid: imdbid,
+                            posters: posters
+                        });
+                    }
+                })
+                .catch(function(err) {console.log(err);});
         }
-    });
+    }
 });
 
 app.get("/about",function(req, res){
